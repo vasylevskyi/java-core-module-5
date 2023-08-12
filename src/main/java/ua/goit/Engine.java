d09860069255cfb6e04b45780de35a5a3b5c7508
Anton Vasylevskyi <vasilevskiy@gmail.com>
2023-08-12 13:32:08 +0300

    Added Task 9

--- src/main/java/ua/goit/Engine.java
+++ src/main/java/ua/goit/Engine.java
@@ -1,9 +1,9 @@
 package ua.goit;
 
-/**Module 5 Tasks 7
+/**Module 5 Tasks 7-9
  * */
 
-public class Engine {
+class Engine {
     private int power;
 
     public int getPower () {
@@ -13,11 +13,42 @@ public class Engine {
         this.power = power;
     }
     public String getFuelType() {
-        String fuelType = "A500";
-        return fuelType;
+        return "A500";
     }
 
     public static void main(String[] args) {
-        System.out.println(new Engine().getFuelType());
+//        System.out.println(new Engine().getFuelType());
+
+/*        Engine basicEngine = new Engine();
+        System.out.println(basicEngine.getFuelType()); //A500
+
+        Engine xFuelEngine = new XFuelEngine();
+        System.out.println(xFuelEngine.getFuelType()); //XFuel*/
+
+        AdvancedXFuelEngine engine = new AdvancedXFuelEngine();
+        engine.setPower(1000);
+        engine.setSerialNumber("SN504030");
+        engine.printInfo(); //Serial number is SN504030, power is 1000
     }
 }
+
+class XFuelEngine extends Engine {
+    protected String serialNumber;
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+    @Override
+    public String getFuelType() {
+        return "XFuel";
+    }
+}
+
+class AdvancedXFuelEngine extends XFuelEngine {
+
+    public void printInfo() {
+        System.out.printf("Serial number is %s, power is %s", serialNumber, getPower());
+    }
+
+}
+
