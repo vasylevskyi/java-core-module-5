fdac936cb90966ed0a81defd0c505bb2bbade1dd
Anton Vasylevskyi <vasilevskiy@gmail.com>
2023-08-12 17:08:34 +0300

    Added Task 12

--- src/main/java/ua/goit/Passenger.java
+++ src/main/java/ua/goit/Passenger.java
@@ -1,9 +1,41 @@
 package ua.goit;
 
-/**Module 5 Tasks 1-
+/**Module 5 Tasks 11-12
  * */
 abstract class Passenger {
     public abstract String getType();
 
     public abstract int getTicketPrice();
+
+    public static void main(String[] args) {
+/*        Passenger regular = new RegularPassenger();
+        System.out.println(regular.getType()); //Regular
+        System.out.println(regular.getTicketPrice()); //199*/
+
+        Passenger vip = new VIPPassenger();
+        System.out.println(vip.getType());  //VIP
+        System.out.println(vip.getTicketPrice()); //399
+    }
+}
+
+class RegularPassenger extends Passenger {
+    @Override
+    public String getType() {
+        return "Regular";
+    }
+    @Override
+    public int getTicketPrice() {
+        return 199;
+    }
+}
+
+class VIPPassenger extends Passenger {
+    @Override
+    public String getType() {
+        return "VIP";
+    }
+    @Override
+    public int getTicketPrice() {
+        return 399;
+    }
 }
