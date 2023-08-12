36645f5270433e0c6a0d40d5d9b6e4063220d8d5
Anton Vasylevskyi <vasilevskiy@gmail.com>
2023-08-12 17:20:15 +0300

    Added Task 13

---
+++ src/main/java/ua/goit/LaunchCalculator.java
@@ -0,0 +1,50 @@
+package ua.goit;
+
+/**Module 5 Tasks 13
+ * */
+
+class LaunchCalculator {
+    public int calculateTotalPrice(Spaceport spaceport, int passengerCount) {
+        return spaceport.calculateTotalPrice(passengerCount);
+    }
+
+    public static void main(String[] args) {
+        LaunchCalculator calculator = new LaunchCalculator();
+
+        int jupiterStarPrice = calculator.calculateTotalPrice(new JupiterStar(), 100);
+        System.out.println("JupiterStar price = " + jupiterStarPrice); //700
+
+        int mercuryBeatPrice = calculator.calculateTotalPrice(new MercuryBeat(), 100);
+        System.out.println("MercuryBeat price = " + mercuryBeatPrice); //1500
+
+        int sunHeavenPrice = calculator.calculateTotalPrice(new SunHeaven(), 100);
+        System.out.println("SunHeaven price = " + sunHeavenPrice); //2000
+    }
+}
+
+abstract class Spaceport {
+    public abstract int calculateTotalPrice(int passengerCount);
+}
+
+class JupiterStar extends Spaceport {
+    @Override
+    public int calculateTotalPrice(int passengerCount) {
+        int totalPrice = 500 + passengerCount * 2;
+        return totalPrice;
+    }
+}
+
+class MercuryBeat extends Spaceport {
+    @Override
+    public int calculateTotalPrice(int passengerCount) {
+        int totalPrice = passengerCount * 15;
+        return totalPrice;
+    }
+}
+
+class SunHeaven extends Spaceport {
+    @Override
+    public int calculateTotalPrice(int passengerCount) {
+        return 2000;
+    }
+}
