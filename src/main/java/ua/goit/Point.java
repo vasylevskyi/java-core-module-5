d80cf4b40d5c5e9da3c0046d6f4cb9be4df299c7
Anton Vasylevskyi <vasilevskiy@gmail.com>
2023-08-12 19:15:20 +0300

    Added task 20 Overrided equals()

--- src/main/java/ua/goit/Point.java
+++ src/main/java/ua/goit/Point.java
@@ -1,5 +1,7 @@
 package ua.goit;
 
+import java.util.Objects;
+
 /**Module 5 Tasks 19
  * */
 
@@ -30,6 +32,19 @@ public class Point {
         this.z = z;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y && z == point.z;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
+    }
+
     public static void main(String[] args) {
         Point p1 = new Point();
         p1.setX(1);
@@ -41,7 +56,13 @@ public class Point {
         p2.setY(1);
         p2.setZ(1);
 
-        System.out.println(p1.equals(p2)); //Will be false
+        Point p3 = new Point();
+        p3.setX(2);
+        p3.setY(2);
+        p3.setZ(3);
+
+        System.out.println(p1.equals(p2)); //Should be true
+        System.out.println(p1.equals(p3)); //Should be false
     }
 }
 
