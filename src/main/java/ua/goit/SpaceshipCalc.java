11227c362447770226b5cd957a779519b454ae48
Anton Vasylevskyi <vasilevskiy@gmail.com>
2023-08-12 17:29:28 +0300

    Added Task 15

---
+++ src/main/java/ua/goit/SpaceshipCalc.java
@@ -0,0 +1,15 @@
+package ua.goit;
+
+/**Module 5 Tasks 15
+ * */
+
+public class SpaceshipCalc {
+    final int calculateAirBalloons(int passengerCount) {
+        return passengerCount * 10;
+    }
+
+    int calculateBookCount(int passengerCount) {
+        return passengerCount * 2;
+
+    }
+}
