d314dbc74d7b227e2de44c1973b73c6be9e1a14f
Anton Vasylevskyi <vasilevskiy@gmail.com>
2023-08-13 16:50:22 +0300

    Added task 28 Factorial recursive and in loop

--- src/main/java/ua/goit/FirTest.java
+++ src/main/java/ua/goit/FirTest.java
@@ -1,5 +1,7 @@
 package ua.goit;
 
+import javax.print.attribute.standard.OrientationRequested;
+
 class FirTest {
     public int test(FirNum firNum, int number) {
         return firNum.calc(number);
@@ -12,11 +14,17 @@ class FirTest {
         //Should be 10
         System.out.println(firTest.test(firNum, 10));*/
 
-        FirNum firNum = new FirNumSum();
+/*        FirNum firNum = new FirNumSum();
         FirTest firTest = new FirTest();
 
         //Should be 6
-        System.out.println(firTest.test(firNum, 3));
+        System.out.println(firTest.test(firNum, 3));*/
+
+        FirNum firNum = new FirNumFactorial();
+        FirTest firTest = new FirTest();
+
+        //Should be 120
+        System.out.println(firTest.test(firNum, 5));
     }
 }
 class FirNum {
@@ -33,5 +41,23 @@ class FirNumSum extends FirNum {
             sum = sum + i;
         }
         return sum;
+    }
+}
+
+class FirNumFactorial extends FirNum {
+    @Override
+    public int calc(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int factorial = n * calc(n - 1);
+        return factorial;
+
+/*        OR
+        int result = 1;
+        for(int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;    */
     }
 }
